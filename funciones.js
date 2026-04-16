3704a1b09000bc587e8b482b07b477db66112e7c
carlosao24 <carloslopez21trabajos@gmail.com>
2026-04-16 17:52:03 -0500

    Mejora visual con IA

--- funciones.js
+++ funciones.js
@@ -17,3 +17,33 @@ function texto(id,vari){
     const elemento = document.getElementById(id);
     elemento.innerText = vari.toFixed(2);
 }
+
+function calcularInteresSimple(monto, tasa, plazoAnios){
+    let interes = plazoAnios * monto * (tasa/100);
+    return interes;
+}
+
+function calcularTotalPagar(monto, interes){
+    let contribucionSOLCA = 100;
+    let totalPagar = monto + interes + contribucionSOLCA;
+    return totalPagar;
+}
+
+function calcularCuotaMensual(total, plazoAnios){
+    let plazoMeses = plazoAnios * 12;
+    let cuotaMensual = total/plazoMeses;
+    return cuotaMensual;
+}
+
+function aprobarCredito(capacidadPago,cuotaMensual){
+    if(capacidadPago > cuotaMensual){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function mostrarResultado(idComponente, texto){
+    let estadoCredito = document.getElementById(idComponente);
+    estadoCredito.textContent = texto;
+}
--- simulador.js
+++ simulador.js
@@ -4,12 +4,34 @@ function calcular() {
 
     let ingresos = parseFloat(document.getElementById("txtIngresos").value);
     let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
     let disponible = calcularDisponible(ingresos, egresos);
-    
     texto("spnDisponible", disponible);
 
     let capacidadPagoDis = calcularCapacidadPago(disponible);
-
     texto("spnCapacidadPago",capacidadPagoDis);
+
+    let monto = parseInt(document.getElementById("txtMonto").value);
+    let tasa = parseInt(document.getElementById("txtTasaInteres").value);
+    let plazoAnios = parseInt(document.getElementById("txtPlazo").value);
+
+    let mostrarInteres = calcularInteresSimple(monto, tasa, plazoAnios);
+    texto("spnInteresPagar", mostrarInteres);
+
+    let totalPagar = calcularTotalPagar(monto,mostrarInteres);
+    texto("spnTotalPrestamo",totalPagar);
+
+    let cuotaMensual = calcularCuotaMensual(totalPagar,plazoAnios);
+    texto("spnCuotaMensual",cuotaMensual);
+
+    let estadoCredito = aprobarCredito(capacidadPagoDis,cuotaMensual);
+    if( estadoCredito == true){
+        mostrarResultado("spnEstadoCredito","CREDITO APROBADO");
+    }else{
+        mostrarResultado("spnEstadoCredito","CREDITO RECHAZADO");
+    }
+    
+    
+    
 
 }
