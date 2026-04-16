aa17dcfb3c6f27c5eab420f7660f0f6cdf2dd319
carlosao24 <carloslopez21trabajos@gmail.com>
2026-04-15 21:33:15 -0500

    Avance de taller

--- funciones.js
+++ funciones.js
@@ -1,1 +1,19 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+
+function calcularDisponible(ingresos, egresos){
+     let disponible = ingresos - egresos;
+    if(disponible < 0){
+     disponible = 0;
+    }
+    return disponible;
+}
+
+function calcularCapacidadPago(montoDisponicle){
+    let capacidadPago = montoDisponicle * 0.5;
+    return capacidadPago;
+}
+
+function texto(id,vari){
+    const elemento = document.getElementById(id);
+    elemento.innerText = vari.toFixed(2);
+}
